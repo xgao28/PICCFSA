ffd4b7072b6514f66efa7886c4359d2252bce07b
Xinxiang <113380522+xgao28@users.noreply.github.com>
2024-09-19 16:17:40 -0400

    Uploaded the initialization for the project
    1. Created some directories
    2. Updated an R script to download the data

---
+++ scripts/01-download_data.R
@@ -0,0 +1,16 @@
+library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("persons-in-crisis-calls-for-service-attended")
+package
+
+# get all resources for this package
+resources <- list_package_resources("persons-in-crisis-calls-for-service-attended")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+write.csv(data, "data/raw_data/unedited_data.csv", row.names = FALSE)
