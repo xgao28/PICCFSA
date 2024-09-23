66fa5dae5d435d80a71b27fb965f4dfff465b121
Xinxiang <113380522+xgao28@users.noreply.github.com>
2024-09-23 13:30:02 -0400

    Cleaned data and added docstrings

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,9 +1,16 @@
+# Purpose: Downloads the data from opendatatoronto API. 
+# Source: https://open.toronto.ca/dataset/persons-in-crisis-calls-for-service-attended/
+# Author: Xinxiang Gao
+# Date: 23 September 2024
+# Contact: xinxiang.gao@mail.utoronto.ca
+# License: MIT
+
+
 library(opendatatoronto)
 library(dplyr)
 
 # get package
 package <- show_package("persons-in-crisis-calls-for-service-attended")
-package
 
 # get all resources for this package
 resources <- list_package_resources("persons-in-crisis-calls-for-service-attended")
@@ -13,4 +20,5 @@ datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'
 
 # load the first datastore resource as a sample
 data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-write.csv(data, "../data/raw_data/raw_piccfsa.csv", row.names = FALSE)
+write.csv(data, "data/raw_data/raw_piccfsa.csv",  row.names = F)
+
---
+++ scripts/02-cleaning_data.R
@@ -0,0 +1,15 @@
+# Purpose: Cleans the raw data after download
+# Author: Xinxiang Gao
+# Date: 23 September 2024
+# Contact: xinxiang.gao@mail.utoronto.ca
+# License: MIT
+
+
+data <- read.csv("data/raw_data/raw_piccfsa.csv")
+
+colnames(data)
+data_1 <- data %>% janitor::clean_names() %>% 
+  select(-x_id, -hood_140, -neighbourhood_140)
+  
+write.csv(data_1, "data/analysis_data/piccfsa.csv", row.names = F)
+  
